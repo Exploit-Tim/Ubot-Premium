f376060cc4b400c24c4e75744b2c3647ec509180
ᴛɪᴍᴇ ɪs ᴍᴏɴᴇʏ <codexyraa@gmail.com>
2025-05-13 20:33:56 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,15 +5,15 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "999"))
 
-DEVS = list(map(int, os.getenv("DEVS", "7463774265").split()))
+DEVS = list(map(int, os.getenv("DEVS", "1927018403").split()))
 
-API_ID = int(os.getenv("API_ID", "21825364"))
+API_ID = int(os.getenv("API_ID", "27418440"))
 
-API_HASH = os.getenv("API_HASH", "f268e8cdb0728ee658d8e5cba3464cb0")
+API_HASH = os.getenv("API_HASH", "0a08a360e0e9f41b9896f655c300d09d")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8150379277:AAFdCxFrnJi84QpeAWIcTXjIMBSjEo-Nyo4")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7883408843:AAGGxg588CdO_3AzO-1WMDd3TksewdwaCEE")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "7463774265"))
+OWNER_ID = int(os.getenv("OWNER_ID", "1927018403"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", " -1002356932965").split()))
 
@@ -21,4 +21,4 @@ RMBG_API = os.getenv("RMBG_API", "f3CYy31QTGdSyYABY8D7FTqQ")
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Unixxpedia:unix2008@cluster0.cn4kypa.mongodb.net/Unixxpedia?retryWrites=true&w=majority&appName=Cluster0")
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002552768474"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002197595899"))
